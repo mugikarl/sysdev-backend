149474a3dd63bc6941e2674d72db0dc30be21d46
karl natividad <kjasnatividad@addu.edu.ph>
2024-07-12 20:58:00 +0800

    Stage 2

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const exp = require('express');
+const mysql = require('mysql');
+
+const connectdb = mysql.createPool({
+    host:"localhost",
+    user:"root",
+    password:"root",
+    database:"sysdev_recruitment" 
+});
+
+//Stage 2
+//Used create database sysdev_recruitment; to create the database
+//Used create table programming_languages(id int not null auto_increment primary key, favorites varchar(255)); to create the table
+//Used insert into programming_languages(favorites) values('Javascript'); to insert values
 
 const web = exp();
 web.get('/',(req,res) =>{
@@ -7,4 +20,5 @@ web.get('/',(req,res) =>{
 
 web.listen(3000);
 
+//Stage 1
 //Used node index.js to run the application
